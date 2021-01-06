796db6382f3ebc06833b86903a58b4b8c740a723
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-06 11:50:14 -0300

    update: calculate the sumOr for the max value

--- src/ex1/main.ts
+++ src/ex1/main.ts
@@ -1,6 +1,13 @@
 class Multiple {
   calculateSumOr(max) {
-    return 23;
+    let sum = 0;
+    for (let i = 0; i < max; i++) {
+      if (i % 3 === 0 || i % 5 === 0) {
+        sum += i;
+      }
+    }
+
+    return sum;
   }
 }
 
