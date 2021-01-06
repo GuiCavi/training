692f1428ebf270de9d802e21f0802f3ed1e6d5cf
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-06 11:49:33 -0300

    text: calculate the sum of 3 OR 5 for 1000

--- src/ex1/main.test.ts
+++ src/ex1/main.test.ts
@@ -5,11 +5,19 @@ const makeSut = () => {
 }
 
 describe("Exercício 1", () => {
-  test("Should not go over 10", () => {
+  test("Should calculate the sum of 3 OR 5 multiples for 10", () => {
     const sut = makeSut();
 
     const result = sut.calculateSumOr(10);
 
     expect(result).toBe(23);
-  })
+  });
+
+  test("Should calculate the sum of 3 OR 5 multiples for 1000", () => {
+    const sut = makeSut();
+
+    const result = sut.calculateSumOr(1000);
+
+    expect(result).toBe(233168);
+  });
 })
