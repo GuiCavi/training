fc6fd7652d67eb60a589a5e3e22e4f983dc76ec2
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-06 16:57:08 -0300

    feat: implement happy numbers function

--- src/ex2/HappyNumbers.ts
+++ src/ex2/HappyNumbers.ts
@@ -1,5 +1,30 @@
+const calculateSum = (num: number) => {
+  let sum = 0;
+
+  while (num > 0) {
+    const digit = num % 10;
+
+    sum += Math.pow(digit, 2);
+    num = Math.floor(num / 10);
+  }
+
+  return sum;
+}
+
 export const isHappyNumber = (num: number) => {
-  if (num === 1) {
+  if (num === 1 || num === 7) {
     return true;
   }
+
+  while (num > 9) {
+    let sum = calculateSum(num);
+
+    if (sum === 1 || sum === 7) {
+      return true;
+    }
+
+    num = sum;
+  };
+
+  return false;
 }
