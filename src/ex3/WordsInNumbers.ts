a1ce4ef8d9637694365bac6dfb196d8e8f15636a
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-07 10:36:38 -0300

    feat: implement isMultiple in execute

--- src/ex3/WordsInNumbers.ts
+++ src/ex3/WordsInNumbers.ts
@@ -1,5 +1,6 @@
+import Multiple from "../ex1/Multiple";
 import { isHappyNumber } from "../ex2/HappyNumbers";
-import { isPrime } from "./PrimeNumber";
+import { isPrime as isPrimeNumber } from "./PrimeNumber";
 
 const LETTERS = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
 
@@ -12,9 +13,16 @@ class WordsInNumbers {
         .map(wordLetter => LETTERS.findIndex(letter => letter === wordLetter))
         .reduce((prev, curr) => prev + curr, 0);
 
+    const multiple = new Multiple();
+
+    const isPrime = isPrimeNumber(sum);
+    const isHappy = isHappyNumber(sum);
+    const isMultiple = multiple.isMultiple(sum, 3) || multiple.isMultiple(sum, 5);
+
     return {
-      isPrime: isPrime(sum),
-      isHappy: isHappyNumber(sum)
+      isPrime,
+      isHappy,
+      isMultiple
     };
   }
 }
