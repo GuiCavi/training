4d9b230121a28bc474dc15cccc8c9f13fb3ba91c
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-06 21:41:12 -0300

    feat: implement isPrime in execute for WordsInNumbers

--- src/ex3/WordsInNumbers.ts
+++ src/ex3/WordsInNumbers.ts
@@ -1,6 +1,19 @@
+import { isPrime } from "./PrimeNumber";
+
+const LETTERS = ".abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
 class WordsInNumbers {
-  isPrime(word) {
-    return false;
+  execute(word) {
+    const sum =
+      word
+        .replace(/[\W|\_|\d]/g, "")
+        .split("")
+        .map(wordLetter => LETTERS.findIndex(letter => letter === wordLetter))
+        .reduce((prev, curr) => prev + curr, 0);
+
+    return {
+      isPrime: isPrime(sum)
+    };
   }
 }
 
