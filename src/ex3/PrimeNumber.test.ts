f7e62acd62791c6e67e226860070f55e485516a4
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-06 21:23:10 -0300

    test: ensure isPrime returns correct values for the given input

--- src/ex3/PrimeNumber.test.ts
+++ src/ex3/PrimeNumber.test.ts
@@ -4,12 +4,68 @@ const makeSut = () => {
   return { isPrime }
 }
 
-describe("Exercício 3", () => {
+describe("Prime Numbers", () => {
+  test("Should check if the number 1 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(1);
+
+    expect(result).toBe(true);
+  });
+
+  test("Should check if the number 2 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(2);
+
+    expect(result).toBe(true);
+  });
+
+  test("Should check if the number 3 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(3);
+
+    expect(result).toBe(true);
+  });
+
+  test("Should check if the number 5 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(5);
+
+    expect(result).toBe(true);
+  });
+
+  test("Should check if the number 17 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(17);
+
+    expect(result).toBe(true);
+  });
+
+  test("Should check if the number 21 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(21);
+
+    expect(result).toBe(false);
+  });
+
   test("Should check if the number 84 is a prime number", () => {
     const sut = makeSut();
 
     const result = sut.isPrime(84);
 
     expect(result).toBe(false);
+  });
+
+  test("Should check if the number 107 is a prime number", () => {
+    const sut = makeSut();
+
+    const result = sut.isPrime(107);
+
+    expect(result).toBe(true);
   });
 })
