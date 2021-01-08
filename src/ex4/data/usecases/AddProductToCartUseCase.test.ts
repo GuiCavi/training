a2ac6cfd3570f24c63b25d727d4b7f77c20bc90e
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-08 11:31:43 -0300

    test: update spy and test case to ensure the product will be save

--- src/ex4/data/usecases/AddProductToCartUseCase.test.ts
+++ src/ex4/data/usecases/AddProductToCartUseCase.test.ts
@@ -2,26 +2,42 @@ import { IProduct } from "../../domain/models/IProduct";
 import { Product } from "../../domain/models/Product";
 import { AddProductToCartUseCase } from "./AddProductToCartUseCase";
 
-const makeSut = () => {
-  class ProductRepositorySpy {
-    save(product: any, qtd: any): boolean {
-      return true;
-    }
+class ProductRepositorySpy {
+  product: IProduct;
+  qtd: number;
 
+  save(product: IProduct, qtd: any): boolean {
+    this.product = product;
+    this.qtd = qtd;
+
+    return true;
   }
+}
 
+type SutType = {
+  sut: AddProductToCartUseCase,
+  productRepositorySpy: ProductRepositorySpy
+}
+
+const makeSut = (): SutType => {
   const productRepositorySpy = new ProductRepositorySpy();
+  const sut = new AddProductToCartUseCase(productRepositorySpy);
 
-  return new AddProductToCartUseCase(productRepositorySpy);
+  return {
+    productRepositorySpy,
+    sut
+  }
 }
 
 describe('Add Product to Cart', () => {
   test('Should have one product after adding 1 product to cart', () => {
-    const sut = makeSut();
+    const { sut, productRepositorySpy } = makeSut();
 
     const product = new Product("Caneta", 5.33);
     const result = sut.add(product, 1);
 
     expect(result).toBe(true)
+    expect(productRepositorySpy.product).toEqual(product);
+    expect(productRepositorySpy.qtd).toBe(1);
   });
 });
