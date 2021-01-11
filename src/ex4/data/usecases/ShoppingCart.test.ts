cb0a8eb79390273c1c3d4486ee7260d2732d337e
Guilherme Cavichioli <guilherme.cavichioli@objective.com.br>
2021-01-11 15:23:23 -0300

    test: ensure cart works fine for the pink path

--- src/ex4/data/usecases/ShoppingCart.test.ts
+++ src/ex4/data/usecases/ShoppingCart.test.ts
@@ -14,7 +14,14 @@ const makeSut = (): SutType => {
 }
 
 describe('Shopping cart', () => {
-  test('Should have 1 product after adding 1 product to cart', () => {
+  test('Should return 0 for the total if no product is in cart', () => {
+    const { sut } = makeSut();
+
+    expect(sut.items.length).toBe(0);
+    expect(sut.total()).toBe(0)
+  });
+
+  test('Should have 1 product after adding 1 product to cart and the total is correct', () => {
     const { sut } = makeSut();
 
     const product = new Product("Caneta", 5.33);
@@ -25,13 +32,41 @@ describe('Shopping cart', () => {
     expect(sut.items.length).toBe(1);
     expect(sut.items[0].qtd).toBe(1);
     expect(sut.items[0].product).toEqual(product);
+    expect(sut.total()).toBe(5.33);
   });
 
-  test('Should ', () => {
+  test('Should have 1 product after adding 5 of the same product to cart and the total is correct', () => {
+    const { sut } = makeSut();
+
+    const products = [
+      new Product("Caneta", 5.33),
+      new Product("Caneta", 5.33),
+      new Product("Caneta", 5.33),
+      new Product("Caneta", 5.33),
+      new Product("Caneta", 5.33),
+    ];
+
+    const result = products.map(product => sut.add(product, 1)).every(value => value);
+
+    expect(result).toBe(true);
+    expect(sut.items.length).toBe(1);
+    expect(sut.items[0].qtd).toBe(5);
+    expect(sut.items[0].product).toEqual(products[0]);
+    expect(sut.total()).toBe(products.length * 5.33);
+  });
+
+  test('Should have 1 product after adding 2 items of 1 product and the total is correct', () => {
     const { sut } = makeSut();
 
     const product = new Product("Caneta", 5.33);
+    const qtdAdded = 2;
 
-    const result = sut.add(product, 1);
+    const result = sut.add(product, qtdAdded);
+
+    expect(result).toBe(true);
+    expect(sut.items.length).toBe(1);
+    expect(sut.items[0].qtd).toBe(qtdAdded);
+    expect(sut.items[0].product).toEqual(product);
+    expect(sut.total()).toBe(qtdAdded * 5.33);
   });
 });
